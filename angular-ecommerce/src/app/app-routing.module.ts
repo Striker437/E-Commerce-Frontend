d99a75ab6cb69950ec8f1fb325ab81ac88ae2ff5
Striker437 <shivam.mmmut@gmail.com>
2020-12-04 22:18:54 +0530

    comment

--- angular-ecommerce/src/app/app-routing.module.ts
+++ angular-ecommerce/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductListbyCategoryComponent } from './components/product-listby-category/product-listby-category.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
  {path: 'product' , component:ProductListbyCategoryComponent},
  {path: 'deleteProduct/:deleteIdp' , component:ProductListbyCategoryComponent},
  {path: 'deleteProductcategorywise/:deleteIdc' , component:ProductListComponent},
+ {path: 'checkout' , component:CheckoutComponent},
  {path: 'cart-details' , component:CartDetailsComponent},
  {path: '' , redirectTo:'/product' ,pathMatch:'full'},
  {path: '**' , redirectTo:'/product',pathMatch:'full'},
